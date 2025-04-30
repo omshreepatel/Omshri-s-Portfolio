3626b5c38acd3bcef4807bc0b3403400a6e6ae6e
OMSHRI <omshreepatel063@gmail.com>
2025-04-30 16:16:42 +0530

    Updated AdminLogin, AdminPanel, and Contact components with new functionality

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/portfolio-backend1': {
+        target: 'https://omshri-portfolio-backend.onrender.com', // Replace with your backend URL
+        changeOrigin: true,
+        secure: false, // Set to true if you're using HTTPS
+      },
+    },
+  },
 })
