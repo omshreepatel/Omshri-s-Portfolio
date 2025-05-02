470e41831cebdbb9f563aee0d593a9dad4ef3116
OMSHRI <omshreepatel063@gmail.com>
2025-05-02 10:09:24 +0530

    Updated AdminLogin components with new functionality

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -1,4 +1,3 @@
-// src/admin/components/AdminLogin.jsx
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
@@ -22,13 +21,13 @@ const AdminLogin = ({ setIsAdminLoggedIn }) => {
     const { username, password } = credentials;
 
     try {
-      // const backendUrl =
-      //   import.meta.env.VITE_BACKEND_URL || "http://localhost:8090";
+      // Get the backend URL from the environment variable
+      const backendUrl =
+        import.meta.env.VITE_BACKEND_URL || "http://localhost:8090";
+      console.log("Backend URL:", backendUrl);
 
-      // const response = await fetch(
-      //   `${backendUrl}/portfolio-backend1/AdminLoginServlet`,
       const response = await fetch(
-        "http://localhost:8090/portfolio-backend1/AdminLoginServlet",
+        `${backendUrl}/portfolio-backend1/AdminLoginServlet`,
         {
           method: "POST",
           headers: {
