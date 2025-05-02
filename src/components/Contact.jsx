c7cf556949886d09193042dc65832ad05dd485cc
OMSHRI <omshreepatel063@gmail.com>
2025-05-02 11:14:19 +0530

    Updated Contact components with new functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -28,6 +28,7 @@ const Contact = () => {
           method: "POST",
           headers: { "Content-Type": "application/x-www-form-urlencoded" },
           body: new URLSearchParams(formData),
+          credentials: "include",
         }
       );
 
